4d441989dbada1cf1cc0fb7c43b38011c6e47415
Piotr Hudziak <hudziakp@gmail.com>
2020-01-21 21:54:36 +0100

    Adding missing file

---
+++ EventsLogger/Controllers/InputOutputController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace EventsLogger.Controllers
+{
+    public class InputOutputController
+    {
+        public void Send(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public int GetColor()
+        {
+            return Console.IsOutputRedirected ? 0 : (int)Console.ForegroundColor;
+        }
+
+        public void SetColor(int color)
+        {
+            if (!Console.IsOutputRedirected)
+                Console.ForegroundColor = (ConsoleColor)color;
+        }
+
+        public string ReadChar()
+        {
+            return Console.ReadKey(true).KeyChar.ToString();
+        }
+    }
+}
